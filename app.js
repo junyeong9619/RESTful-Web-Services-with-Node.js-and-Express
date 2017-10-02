5ca0568e35bac2de1a88aab051d627f725d9dac2
Lee Jun Yeong <leejunyeong@lees-mbp.woo.sing.seagate.com>
2017-10-02 19:12:30 +0800

    Finished Course

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var db;
+if (process.env.ENV == 'Test') {
+    db = mongoose.connect('mongodb://localhost/bookAPI_test', { useMongoClient: true });
+} else {
+    db = mongoose.connect('mongodb://localhost/bookAPI', { useMongoClient: true });
+}
+
+var bookRouter = require('./routes/bookRoutes')();
+
+app.use('/api/books', bookRouter);
 app.get('/', function (req, res) {
     res.send('Welcome to my API');
 });
@@ -10,3 +25,5 @@ app.get('/', function (req, res) {
 app.listen(port, function () {
     console.log('Gulp is running my app on PORT: ' + port);
 });
+
+module.exports = app;
---
+++ routes/bookRoutes.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var Book = require('../models/bookModel');
+
+var routes = function () {
+    var bookRouter = express.Router();
+    var bookController = require('../controllers/bookController')(Book);
+    bookRouter.route('/')
+        .get(bookController.get)
+        .post(bookController.post);
+    bookRouter.use('/:bookId', function (req, res, next) {
+        Book.findById(req.params.bookId, function (err, book) {
+            if (err) {
+                res.status(500).send(err);
+            } else if (book) {
+                req.book = book;
+                next();
+            } else {
+                res.status(404).send('No book found');
+            }
+        });
+    });
+    bookRouter.route('/:bookId')
+        .get(function (req, res) {
+            var returnBook = req.book.toJSON();
+            returnBook.links = {};
+            var newLink = ('http://' + req.headers.host + '/api/books?genre=' + returnBook.genre);
+            returnBook.links.FilterByThisGenre = newLink.replace(' ', '%20');
+            res.json(returnBook);
+        })
+        .put(function (req, res) {
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.genre = req.body.genre;
+            req.book.read = req.body.read;
+            req.book.save(function (err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.book);
+                }
+            });
+        })
+        .patch(function (req, res) {
+            if (req.body._id) {
+                delete req.body._id;
+            }
+            for (var p in req.body) {
+                req.book[p] = req.body[p];
+            }
+            req.book.save(function (err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.book);
+                }
+            });
+        })
+        .delete(function (req, res) {
+            req.book.remove(function (err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.status(204).send('Removed');
+                }
+            });
+        });
+    return bookRouter;
+};
+
+module.exports = routes;
